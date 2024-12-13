f84a9fef7f804fa1d573d2cbac9335c6a041e9de
Gurjot <gurjotsyd@gmail.com>
2024-12-13 12:35:11 +0530

    pr comment: rm unneeded fields in delegation document

--- internal/db/model/delegation.go
+++ internal/db/model/delegation.go
@@ -14,9 +14,7 @@ type DelegationDocument struct {
 	StakingTxHashHex      string                `bson:"_id"` // Primary key
 	StakerPkHex           string                `bson:"staker_pk_hex"`
 	FinalityProviderPkHex string                `bson:"finality_provider_pk_hex"`
-	StakingValue          uint64                `bson:"staking_value"`
 	State                 types.DelegationState `bson:"state"`
 	StakingTx             *TimelockTransaction  `bson:"staking_tx"` // Always exist
 	UnbondingTx           *TimelockTransaction  `bson:"unbonding_tx,omitempty"`
-	IsOverflow            bool                  `bson:"is_overflow"`
 }
