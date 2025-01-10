8f94b8a68c1ba2395a50e4d0cb854b8b4278f188
Gurjot <gurjotsyd@gmail.com>
2025-01-10 14:09:00 +0530

    fix

--- internal/config/poller.go
+++ internal/config/poller.go
@@ -8,15 +8,9 @@ import (
 	"github.com/rs/zerolog"
 )
 
-const (
-	MinBatchSize = 200
-	MaxBatchSize = 2000
-)
-
 type PollerConfig struct {
-	Interval  time.Duration `mapstructure:"interval"`
-	Timeout   time.Duration `mapstructure:"timeout"`
-	BatchSize int64         `mapstructure:"batch-size"`
+	Interval time.Duration `mapstructure:"interval"`
+	Timeout  time.Duration `mapstructure:"timeout"`
 }
 
 type PollersConfig struct {
@@ -48,10 +42,6 @@ func (cfg *PollerConfig) Validate() error {
 
 	if cfg.Timeout <= 0 {
 		return errors.New("poll timeout must be greater than 0")
-	}
-
-	if cfg.BatchSize < MinBatchSize || cfg.BatchSize > MaxBatchSize {
-		return fmt.Errorf("batch size must be between %d and %d", MinBatchSize, MaxBatchSize)
 	}
 
 	return nil
