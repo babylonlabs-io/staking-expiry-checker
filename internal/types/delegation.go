8d7c3fabd57cc3c4e3ba08905c33bf08b22768fb
Kirill <paltsev.kir@gmail.com>
2025-04-22 02:31:53 +0400

    Remove unused code

--- internal/types/delegation.go
+++ internal/types/delegation.go
@@ -1,7 +1,5 @@
 package types
 
-import "fmt"
-
 type DelegationState string
 
 const (
@@ -15,23 +13,4 @@ const (
 
 func (s DelegationState) ToString() string {
 	return string(s)
-}
-
-func FromStringToDelegationState(s string) (DelegationState, error) {
-	switch s {
-	case "active":
-		return Active, nil
-	case "unbonding_requested":
-		return UnbondingRequested, nil
-	case "unbonding":
-		return Unbonding, nil
-	case "unbonded":
-		return Unbonded, nil
-	case "withdrawn":
-		return Withdrawn, nil
-	case "transitioned":
-		return Transitioned, nil
-	default:
-		return "", fmt.Errorf("invalid delegation state: %s", s)
-	}
 }
